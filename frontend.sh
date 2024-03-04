6896c054b11fe7d9903f95e690743d4adae492ef
prkhar <prkharpatel20@gmail.com>
2024-03-05 00:51:39 +1100

    added function of print task headings

--- frontend.sh
+++ frontend.sh
@@ -1,35 +1,41 @@
 #Front End Script
 source common.sh
-echo "install nginx"
+print_task_heading() {
+  echo $1
+  echo "################$1#############" &>>$LOG
+}
+
+
+print_task_heading "install nginx"
 dnf install nginx -y &>>$LOG
 echo $?
 
-echo "enable nginx"
+print_task_heading "enable nginx"
 systemctl enable nginx &>>$LOG
 echo $?
 
-echo "start nginx"
+print_task_heading "start nginx"
 systemctl start nginx &>>$LOG
 echo $?
 
-echo "removing static html content from nginx"
+print_task_heading "removing static html content from nginx"
 rm -rf /usr/share/nginx/html/* &>>$LOG
 echo $?
 
-echo "copying expense.conf file"
+print_task_heading "copying expense.conf file"
 cp expense.conf /etc/nginx/default.d/expense.conf &>>$LOG
 echo $?
 
-echo "downloanding frontend.zip"
+print_task_heading "downloanding frontend.zip"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip &>>$LOG
 cd /usr/share/nginx/html &>>$LOG
 echo $?
 
-echo "unzip frontend"
+print_task_heading "unzip frontend"
 unzip /tmp/frontend.zip &>>$LOG
 echo $?
 
-echo "restart of nginx"
+print_task_heading "restart of nginx"
 systemctl restart nginx &>>$LOG
 echo $?
 
