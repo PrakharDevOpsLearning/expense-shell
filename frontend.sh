c52618d3a30dffed8c673e938f8149f7a3862c0e
prkhar <prkharpatel20@gmail.com>
2024-03-04 17:59:05 +1100

    Creating updating Frontend.sh with reverse proxy details and mysql.sh and backend.sh and added backend.service & expense.conf file

--- frontend.sh
+++ frontend.sh
@@ -1,3 +1,5 @@
+#Front End Script
+
 dnf install nginx -y
 systemctl enable nginx
 systemctl start nginx
@@ -5,4 +7,29 @@ rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
+cp expense.conf /etc/nginx/default.d/expense.conf
 systemctl restart nginx
+
+#MySql Script
+
+dnf install mysql-server -y
+systemctl enable mysqld
+systemctl start mysqld
+mysql_secure_installation --set-root-pass ExpenseApp@1
+
+#BackEnd Script
+dnf module disable nodejs -y
+dnf module enable nodejs:20 -y
+dnf install nodejs -y
+useradd expense
+mkdir /app
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip
+cd /app
+unzip /tmp/backend.zip
+cd /app
+npm install
+cp backend.service /etc/systemd/system/backend.service
+systemctl enable backend
+systemctl start backend
+dnf install mysql -y
+mysql -h 172.31.19.1 -uroot -pExpenseApp@1 < /app/schema/backend.sql
