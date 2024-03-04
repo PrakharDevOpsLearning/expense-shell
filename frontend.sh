a9b249415a1ad8b5923de9d5715abf415451fb68
prkhar <prkharpatel20@gmail.com>
2024-03-04 22:51:51 +1100

    added logic to record logs

--- frontend.sh
+++ frontend.sh
@@ -4,32 +4,32 @@ dnf install nginx -y >/tmp/expense.log
 echo $?
 
 echo "enable nginx"
+systemctl enable nginx >/tmp/expense.log
 echo $?
-systemctl enable nginx
 
 echo "start nginx"
-systemctl start nginx
+systemctl start nginx >/tmp/expense.log
 echo $?
 
 echo "removing static html content from nginx"
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* >/tmp/expense.log
 echo $?
 
 echo "copying expense.conf file"
 pwd
-cp expense.conf /etc/nginx/default.d/expense.conf
+cp expense.conf /etc/nginx/default.d/expense.conf >/tmp/expense.log
 echo $?
 
 echo "downloanding frontend.zip"
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip
-cd /usr/share/nginx/html
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip >/tmp/expense.log
+cd /usr/share/nginx/html >/tmp/expense.log
 echo $?
 
 echo "unzip frontend"
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip >/tmp/expense.log
 echo $?
 
 echo "restart of nginx"
-systemctl restart nginx
+systemctl restart nginx >/tmp/expense.log
 echo $?
 
