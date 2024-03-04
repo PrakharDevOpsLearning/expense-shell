f883c019d6a01fa296e82b208f88d4f0046ff099
prkhar <prkharpatel20@gmail.com>
2024-03-04 21:53:55 +1100

    changed the location of code copying the backend.service & expense.conf file

--- frontend.sh
+++ frontend.sh
@@ -1,55 +1,98 @@
 #Front End Script
-echo "Install nginx"
+echo "install nginx"
 dnf install nginx -y
+echo $?
+
 echo "enable nginx"
+echo $?
 systemctl enable nginx
+
 echo "start nginx"
 systemctl start nginx
+echo $?
+
 echo "removing static html content from nginx"
 rm -rf /usr/share/nginx/html/*
+echo $?
+
+echo "copying expense.conf file"
+cp expense.conf /etc/nginx/default.d/expense.conf
+echo $?
+
 echo "downloanding frontend.zip"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip
 cd /usr/share/nginx/html
+echo $?
+
+echo "unzip frontend"
 unzip /tmp/frontend.zip
-echo "copying expense.conf file"
-cp expense.conf /etc/nginx/default.d/expense.conf
+echo $?
+
 echo "restart of nginx"
 systemctl restart nginx
+echo $?
 
 #MySql Script
+
 echo "install mysql service"
 dnf install mysql-server -y
+echo $?
+
 echo "enable mysql id"
 systemctl enable mysqld
+echo $?
+
 echo "start mysqlid"
 systemctl start mysqld
+echo $?
+
 echo "set password"
 mysql_secure_installation --set-root-pass ExpenseApp@1
+echo $?
 
 #BackEnd Script
 echo "disbale nods.js"
 dnf module disable nodejs -y
+echo $?
+
 echo "enable node.js"
 dnf module enable nodejs:20 -y
-dbf "install nodejs"
+echo $?
+
+echo "install node.js"
 dnf install nodejs -y
+echo $?
+
 echo "adding user"
 useradd expense
+echo $?
+
 echo "copying backend.service"
 cp backend.service /etc/systemd/system/backend.service
-mkdir /app
+
+
 echo "downloading backend.zip"
+mkdir /app
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip
 cd /app
 unzip /tmp/backend.zip
-cd /app
+
 echo "npm install"
 npm install
+echo $?
+
 echo "enable backend"
 systemctl enable backend
+echo $?
+
 echo "start backend"
 systemctl start backend
+echo $?
+
 echo "install mqsql"
 dnf install mysql -y
+echo $?
+
 echo "schema add"
 mysql -h 172.31.19.1 -uroot -pExpenseApp@1 < /app/schema/backend.sql
+echo $?
