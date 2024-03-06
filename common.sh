ac423269a5316a7339eccb1d06079f3900fa86e4
prkhar <prkharpatel20@gmail.com>
2024-03-07 00:35:37 +1100

    checking If condition

--- common.sh
+++ common.sh
@@ -3,3 +3,12 @@ print_task_heading() {
   echo $1
   echo "################$1#############" &>>$LOG
 }
+
+check_status() {
+  if [ $1 -eq 0 ]; then
+    echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\e[31mFAILURE\e[0m"
+    exit 2
+  fi
+}
--- frontend.sh
+++ frontend.sh
@@ -3,34 +3,34 @@ source common.sh
 
 print_task_heading "install nginx"
 dnf install nginx -y &>>$LOG
-echo $?
+check_status $?
 
 print_task_heading "enable nginx"
 systemctl enable nginx &>>$LOG
-echo $?
+check_status $?
 
 print_task_heading "start nginx"
 systemctl start nginx &>>$LOG
-echo $?
+check_status $?
 
 print_task_heading "removing static html content from nginx"
 rm -rf /usr/share/nginx/html/* &>>$LOG
-echo $?
+check_status $?
 
 print_task_heading "copying expense.conf file"
 cp expense.conf /etc/nginx/default.d/expense.conf &>>$LOG
-echo $?
+check_status $?
 
 print_task_heading "downloanding frontend.zip"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip &>>$LOG
 cd /usr/share/nginx/html &>>$LOG
-echo $?
+check_status $?
 
 print_task_heading "unzip frontend"
 unzip /tmp/frontend.zip &>>$LOG
-echo $?
+check_status $?
 
 print_task_heading "restart of nginx"
 systemctl restart nginx &>>$LOG
-echo $?
+check_status $?
 
