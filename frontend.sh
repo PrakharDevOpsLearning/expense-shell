9ad9351c52949b217edfce0d19d9d7ec52341a24
prkhar <prkharpatel20@gmail.com>
2024-03-09 16:53:49 +1100

    checking If condition

--- frontend.sh
+++ frontend.sh
@@ -8,19 +8,21 @@ print_task_heading "install nginx"
 dnf install nginx -y &>>$LOG
 check_status $?
 
-print_task_heading "enable nginx"
-systemctl enable nginx &>>$LOG
-check_status $?
-
-print_task_heading "start nginx"
-systemctl start nginx &>>$LOG
-check_status $?
-
 print_task_heading "copying expense.conf file"
 cp expense.conf /etc/nginx/default.d/expense.conf &>>$LOG
 check_status $?
 
 AppPreReq
+
+print_task_heading "enable nginx"
+systemctl enable nginx &>>$LOG
+check_status $?
+
+<<EOF
+print_task_heading "start nginx"
+systemctl start nginx &>>$LOG
+check_status $?
+EOF
 
 print_task_heading "restart of nginx"
 systemctl restart nginx &>>$LOG
